9ccf35beae1ba0002ac7f2edb0b9fb76801741e5
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-23 10:49:54 +0530

    authSlice is created

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { user: null, token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logout: (state, action) => {
+      state.auth = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logout } = authSlice.actions;
+
+export const selectCurrentUser = (state) => state.auth.user;
+export const selectCurrentToken = (state) => state.auth.token;
+
+export default authSlice.reducer;
